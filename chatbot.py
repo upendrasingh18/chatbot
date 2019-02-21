2ddce691ddf5d006359d484f717514ff7ed4de6e
Upendra Singh Karmiyal <40133779+upendrasingh18@users.noreply.github.com>
2019-02-21 11:55:02 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,22 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import os
+
+bot = ChatBot('Bot')
+bot.set_trainer(ListTrainer)
+
+for files in os.listdir('C:/Users/Dell/Downloads/24 SEPTEMBER chatbot/chatterbot-corpus-master/chatterbot_corpus/data/english/'):
+        data = open('C:/Users/Dell/Downloads/24 SEPTEMBER chatbot/chatterbot-corpus-master/chatterbot_corpus/data/english/' + files ,'r').readlines()
+        bot.train(data)
+
+while True:
+    message = input('you:')
+    if message.strip() == ' Bye':
+        reply = bot.get_response(message)
+        print('ChatBot : ',reply)
+    if message .strip()== 'Bye':
+        print('ChatBot : Bye ')
+        break
+    
+        
+ 
